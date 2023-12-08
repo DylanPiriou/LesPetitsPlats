6598dcf48e754055d91e5a0c12b6df7ce07ddc95
DylanPiriou <dylan.piriou@hotmail.com>
2023-12-08 14:58:10 +0100

    feat: native search method

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,31 +1,51 @@
 import { recipes } from "./recipes.js";
 
-// Fonction recherche 1
+// Fonction recherche 2
 export function filterRecipesByValue(value) {
-    console.log(value);
+    const filteredRecipes = [];
 
-    const filteredRecipes = recipes.filter(recipe => {
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
         const lowerCaseValue = value.toLowerCase();
 
+        let nameMatch = false;
+        let ingredientsMatch = false;
+        let ustensilsMatch = false;
+        let applianceMatch = false;
+
         // Vérifier si la valeur est présente dans le nom de la recette
-        const nameMatch = recipe.name.toLowerCase().includes(lowerCaseValue);
+        if (recipe.name.toLowerCase().includes(lowerCaseValue)) {
+            nameMatch = true;
+        }
 
         // Vérifier si la valeur est présente dans les ingrédients de la recette
-        const ingredientsMatch = recipe.ingredients.some(ingredient =>
-            ingredient.ingredient.toLowerCase().includes(lowerCaseValue)
-        );
+        for (let j = 0; j < recipe.ingredients.length; j++) {
+            const ingredient = recipe.ingredients[j].ingredient;
+            if (ingredient.toLowerCase().includes(lowerCaseValue)) {
+                ingredientsMatch = true;
+                break;
+            }
+        }
 
         // Vérifier si la valeur est présente dans les ustensiles de la recette
-        const ustensilsMatch = recipe.ustensils.some(ustensil =>
-            ustensil.toLowerCase().includes(lowerCaseValue)
-        );
+        for (let k = 0; k < recipe.ustensils.length; k++) {
+            const ustensil = recipe.ustensils[k];
+            if (ustensil.toLowerCase().includes(lowerCaseValue)) {
+                ustensilsMatch = true;
+                break;
+            }
+        }
 
         // Vérifier si la valeur est présente dans la propriété appliance de la recette
-        const applianceMatch = recipe.appliance.toLowerCase().includes(lowerCaseValue);
+        if (recipe.appliance.toLowerCase().includes(lowerCaseValue)) {
+            applianceMatch = true;
+        }
 
         // Retourner true si la valeur est présente dans au moins une propriété de la recette
-        return nameMatch || ingredientsMatch || ustensilsMatch || applianceMatch;
-    });
+        if (nameMatch || ingredientsMatch || ustensilsMatch || applianceMatch) {
+            filteredRecipes.push(recipe);
+        }
+    }
 
-        return filteredRecipes;
+    return filteredRecipes;
 }
