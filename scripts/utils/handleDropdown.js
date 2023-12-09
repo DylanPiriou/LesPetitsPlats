7a9a3ff02e51af4459c4bd5d9e413c3bd0668a0e
DylanPiriou <dylan.piriou@hotmail.com>
2023-12-09 02:42:05 +0100

    fea(ux): improved dropdown closure management

--- scripts/utils/handleDropdown.js
+++ scripts/utils/handleDropdown.js
@@ -1,9 +1,39 @@
-export function handleDropdown (titles, contents, chevrons) {
-    // Ajout d'un gestionnaire d'événement pour chaque titre de dropdown
-    titles.forEach((title, index) => {
-        title.addEventListener("click", () => {
-            contents[index].classList.toggle("active");
-            chevrons[index].classList.toggle("active");
-        });
-    });
+export function handleDropdown(titles, contents, chevrons) {
+	// Ajout d'un gestionnaire d'événement pour chaque titre de dropdown
+	titles.forEach((title, index) => {
+		title.addEventListener("click", (event) => {
+			// Empêche l'événement de se propager au document
+			event.stopPropagation();
+
+			// Ferme tous les autres dropdowns
+			contents.forEach((content, i) => {
+				if (i !== index && content.classList.contains("active")) {
+					content.classList.remove("active");
+					chevrons[i].classList.remove("active");
+				}
+			});
+
+			// Ouvre ou ferme le dropdown cliqué
+			contents[index].classList.toggle("active");
+			chevrons[index].classList.toggle("active");
+		});
+	});
+
+	// Ajout d'un gestionnaire d'événement pour chaque contenu de dropdown
+	contents.forEach((content) => {
+		content.addEventListener("click", (event) => {
+			// Empêche l'événement de se propager au document
+			event.stopPropagation();
+		});
+	});
+
+	// Ajout d'un gestionnaire d'événement au document pour fermer les dropdowns
+	document.addEventListener("click", () => {
+		contents.forEach((content, index) => {
+			if (content.classList.contains("active")) {
+				content.classList.remove("active");
+				chevrons[index].classList.remove("active");
+			}
+		});
+	});
 }
